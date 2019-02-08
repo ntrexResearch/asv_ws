3b6b0c94029293f5b1e0e455e1b70c30dadfe53c
Ntrex <jun928@ntrex.co.kr>
2019-02-08 17:40:57 +0900

    remote

--- asv_lane_track/src/asv_lane_detection.py
+++ asv_lane_track/src/asv_lane_detection.py
@@ -1,16 +1,21 @@
 #! /usr/bin/env python
+from __future__ import division
 
+from collections import deque
 from image_preprocessor import ImagePreprocessor
 from sensor_msgs.msg import Image
 from std_msgs.msg import Float64
 from std_srvs.srv import Empty, EmptyResponse
 from cv_bridge import CvBridge, CvBridgeError
+import numpy as np
 import cv2
 import rospy
 
+QUEUE_LENGTH=50
+
 NODE_NAME = "lane_detection_node"
 IMG_SUB_TOPIC = "/camera/image_raw"
-IMG_PUB_TOPIC = "/camera/canny"
+IMG_PUB_TOPIC = "/camera/lane"
 class LaneDetector:
     def __init__(self, node_name, sub_topic, pub_topic):
         self.bridge = CvBridge()
@@ -22,12 +27,12 @@ class LaneDetector:
         rospy.init_node(node_name)
 
         # Crop parameters
-        self.above = rospy.get_param("/asv_lane_track/lane_detection_node/above", 0.58)
+        self.above = rospy.get_param("/asv_lane_track/lane_detection_node/above", 0.35)
         self.below = rospy.get_param("/asv_lane_track/lane_detection_node/below", 0.1)
-        self.side = rospy.get_param("/asv_lane_track/lane_detection_node/side", 0.3)
+        self.side = rospy.get_param("/asv_lane_track/lane_detection_node/side", 0)
 
         # Lane tracking parameters
-        self.deviation = rospy.get_param("/asv_lane_track/lane_detection_node/deviation", 5)
+        self.deviation = rospy.get_param("/asv_lane_track/lane_detection_node/deviation", 15)
         self.border = rospy.get_param("/asv_lane_track/lane_detection_node/border", 0)
         # Canny parameters
         self.low_thresh = rospy.get_param("/asv_lane_track/lane_detection_node/low_threshold", 50)
@@ -36,7 +41,46 @@ class LaneDetector:
 
         self.image_sub = rospy.Subscriber(sub_topic, Image, self.img_callback)
         self.image_pub = rospy.Publisher(pub_topic, Image, queue_size=5)
+
+        self.yellow_masked_pub = rospy.Publisher("/yellow_masked", Image, queue_size=5)
+
+        self.gray_pub = rospy.Publisher("/test", Image, queue_size=5)
+        self.blur_pub = rospy.Publisher("/blur", Image, queue_size=5)
+
+        self.left_lines = deque(maxlen=QUEUE_LENGTH)
+        self.right_lines = deque(maxlen=QUEUE_LENGTH)
         rospy.spin()
+
+    def process(self, img):
+        cropped = self.img_proc.crop(img, self.above, self.below, self.side)
+        mask = cropped
+        yellow_mask = self.img_proc.select_yellow(mask)
+        yellow_img = cv2.bitwise_and(cropped, cropped, mask=yellow_mask)
+        gray = self.img_proc.convert_gray(yellow_img)
+        smooth_gray = self.img_proc.blur(gray, (self.deviation, self.deviation), self.border)
+        edges = self.img_proc.edge_detection(smooth_gray, self.low_thresh, self.high_thresh, self.aperture)
+        lines = self.img_proc.hough_lines(edges)
+        # print(lines)
+
+        # return self.img_proc.draw_lines_from_points(cropped, lines)
+
+        left_line, right_line = self.img_proc.lane_lines(cropped, lines)
+
+        def mean_line(line, lines):
+            if line is not None:
+                lines.append(line)
+
+            if len(lines) > 0:
+                line = np.mean(lines, axis=0, dtype=np.int32)
+                line = tuple(map(tuple, line))  # make sure it's tuples not numpy array for cv2.line to work
+            return line
+
+        left_line = mean_line(left_line, self.left_lines)
+        right_line = mean_line(right_line, self.right_lines)
+
+        middle_line = ((int(left_line[0][0] + (right_line[0][0] - left_line[0][0]) / 2), int(left_line[0][1])),
+                       (int(left_line[1][0] + (right_line[1][0] - left_line[1][0]) / 2), int(left_line[1][1])))
+        return self.img_proc.draw_lane_lines(cropped, (left_line, right_line, middle_line))
 
     def img_callback(self, data):
         try:
@@ -44,29 +88,47 @@ class LaneDetector:
         except CvBridgeError as e:
             rospy.logerr(e)
 
+        # img_proc = cv_img
+        # # crop
+        # cropped = self.img_proc.crop(img_proc, self.above, self.below, self.side)
+        #
+        # # grayscale
+        # #gray =  self.img_proc.grayscale(cropped)
+        #
+        # # Extract yellow color in hsv
+        # yellow_masked = self.img_proc.select_yellow(cropped)
+        # res = cv2.bitwise_and(cropped,cropped, mask = yellow_masked)
+        #
+        # # blur
+        # # uhm why do you choose
+        # blurred = self.img_proc.blur(res, (self.deviation, self.deviation), self.border)
+        #
+        # # canny
+        # canny =  self.img_proc.edge_detection(blurred, self.low_thresh, self.high_thresh, self.aperture)
+        # # cv2.imshow("canny", canny)
+        # # cv2.waitKey(25)
+        # #canny
+        # lines = self.img_proc.hough_lines(canny)
+        # left_line, right_line = self.img_proc.lane_lines(cv_img, lines)
+        # final = self.img_proc.draw_lane_lines(cv_img, (left_line, right_line))
+        # for line in lines:
+        #     self.img_proc.draw_lane_lines()
 
-        # crop
-        cropped = self.img_proc.crop(cv_img, self.above, self.below, self.side)
 
-        # grayscale
-        gray =  self.img_proc.grayscale(cropped)
-
-        # blur
-        # uhm why do you choose
-        blurred = self.img_proc.blur(gray, (self.deviation, self.deviation), self.border)
-
-        # canny
-        canny =  self.img_proc.edge_detection(blurred, self.low_thresh, self.high_thresh, self.aperture)
-        # cv2.imshow("canny", canny)
-        # cv2.waitKey(25)
-        #canny
+        lane_img = self.process(cv_img)
 
         try:
-            self.image_pub.publish(self.bridge.cv2_to_imgmsg(canny))
+            self.image_pub.publish(self.bridge.cv2_to_imgmsg(lane_img, encoding="rgb8"))
+            #self.gray_pub.publish(self.bridge.cv2_to_imgmsg(cv_img))
+            #cv2.imshow('mask', cv_img)
+            # cv2.imshow('mask', res)
+            #
+            #cv2.waitKey(25)
+            #self.yellow_masked_pub.publish(self.bridge.cv2_to_imgmsg(yellow_masked, encoding="rgb8"))
 
         except CvBridgeError as e:
             rospy.logerr(e)
-        height, width = canny.shape
+        # height, width = canny.shape
 
         # Lane Detection
 
--- asv_lane_track/src/image_preprocessor.py
+++ asv_lane_track/src/image_preprocessor.py
@@ -48,21 +48,88 @@ class ImagePreprocessor:
     def convert_hls(self, img):
         return cv2.cvtColor(img, cv2.COLOR_RGB2HLS)
 
-    def select_white_yellow(self, img):
-        converted = self.convert_hls(img)
-        # white color mask
-        lower = np.uint8([0, 255, 0])
-        upper = np.uint8([255,255,255])
-        white_mask= cv2.inRange(converted, lower, upper)
+    def convert_gray(self, img):
+        return cv2.cvtColor(img, cv2.COLOR_RGB2GRAY)
 
+    def select_yellow(self, img):
+        converted_hsv = self.convert_hsv(img)
+        #print(hue_mask.shape)
         #yellow mask
-        lower = np.uint8([0, 255, 0])
-        upper = np.uint8([255, 255, 255])
-        yellow_mask = cv2.inRange(converted, lower, upper)
-        mask = cv2.bitwise_or(white_mask, yellow_mask)
-        return cv2.bitwise_and(img, img, mask=mask)
+        lower = (80, 30, 100) #cv2.cv.Scalar(20, 100, 100)
+        upper = (100, 200, 255) #cv2.cv.Scalar(30, 255, 255)
+        #lower = np.uint8([20, 100, 100])
+        #upper = np.uint8([30, 255, 255])
+        yellow_mask = cv2.inRange(converted_hsv, lower, upper)
+        # gray = cv2.cvtColor(yellow_mask, cv2.COLOR_HSV2GRAY)
+        #cv2.imwrite("~/yellow_mas.png",yellow_mask)
+        #print(yellow_mask.shape)
+        # mask = cv2.bitwise_or(white_mask, yellow_mask)
+        #return yellow_mask
+        return yellow_mask # cv2.bitwise_and(img, img, mask=gray)
 
 
     def hough_lines(self, img):
         return cv2.HoughLinesP(img, rho=1, theta=np.pi/180, threshold=20, minLineLength=20, maxLineGap=300)
 
+
+    def average_slope_intercept(self, lines):
+        left_lines = [] #(slope, intercept)
+        left_weights = [] # length
+        right_lines = []
+        right_weights = []
+        if lines is None:
+            return None, None
+        for line in lines:
+            for x1, y1, x2, y2 in line:
+                if x2 == x1:
+                    continue
+                slope = (y2 - y1) / (x2 - x1)
+                intercept = y1 - slope*x1
+                length = np.sqrt((y2 - y1)**2 + (x2 - x1)**2)
+                if slope < 0:
+                    left_lines.append((slope, intercept))
+                    left_weights.append((length))
+
+                else:
+                    right_lines.append((slope, intercept))
+                    right_weights.append((length))
+        left_lane = np.dot(left_weights, left_lines) / np.sum(left_weights) if len(left_weights) > 0 else None
+        right_lane = np.dot(right_weights, right_lines) / np.sum(right_weights) if len(right_weights) > 0 else None
+
+        return left_lane, right_lane #in shape (slope, intercept), (slope, intercept)
+
+    def make_line_points(self, y1, y2, line):
+        if line is None:
+            return None
+        slope, intercept = line
+
+        x1 = int((y1 - intercept)/slope)
+        x2 = int((y2 - intercept)/slope)
+        y1 = int(y1)
+        y2 = int(y2)
+        return ((x1, y1), (x2, y2))
+
+    def lane_lines(self, img, lines):
+        left_lane, right_lane = self.average_slope_intercept(lines)
+
+        y1 = img.shape[0]
+        y2 = y1 * 0.4
+
+        left_line = self.make_line_points(y1, y2, left_lane)
+        right_line = self.make_line_points(y1, y2, right_lane)
+
+        return left_line, right_line
+
+    def draw_lane_lines(self, img, lines, color=[255, 0, 0], thickness = 20):
+        line_image = np.zeros_like(img)
+        for line in lines:
+            if line is not None:
+                cv2.line(line_image, line[0], line[1], color, thickness)
+        return cv2.addWeighted(img, 1.0, line_image, 0.95, 0.0)
+
+    def draw_lines_from_points(self, img, lines, color=[255, 0, 0], thickness = 20):
+        line_image = np.zeros_like(img)
+        for line in lines:
+            if line is not None:
+                cv2.line(line_image, (line[0][0], line[0][1]), (line[0][2], line[0][3]), color, thickness)
+        return cv2.addWeighted(img, 1.0, line_image, 0.95, 0.0)
