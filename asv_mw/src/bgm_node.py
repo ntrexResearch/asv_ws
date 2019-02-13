fd9a92f67c3bf7039ad93dd5846d53e888231f5a
Ntrex <jun928@ntrex.co.kr>
2019-02-13 20:30:12 +0900

    remote

--- asv_mw/src/bgm_node.py
+++ asv_mw/src/bgm_node.py
@@ -5,43 +5,44 @@ import rospy
 import actionlib
 from sound_play.msg import SoundRequest, SoundRequestAction, SoundRequestGoal, SoundRequestResult, SoundRequestFeedback
 
+from asv_mw.msg import Asv_state
 import os
 from std_msgs.msg import Int8
 
 worship_bgm = "/worship.wav"
 fault_sound = "/say-beep.wav"
 client = None
-test =0
-def done_cb(goalState):
-    print ''
+bgm_signal =0
+
 
 def feedback_cb(data):
-    global test
+    global bgm_signal
     global client
-    if test:
+    if not bgm_signal:
         client.cancel_goal()
     #print(data)
 
 
-def test_cb(data):
-    global test
-    if data.data is 1:
-        test = 1
-    else:
-        test = 0
+def state_cb(data):
+    global bgm_signal
+    if data.mode is 1:
+        bgm_signal = 1
+    elif data.mode is 0:
+        bgm_signal = 0
+
 
 def sound_play_client(volume=1.0):
     global client
     client = actionlib.SimpleActionClient('sound_play', SoundRequestAction)
 
     client.wait_for_server()
-    sub = rospy.Subscriber('/test', Int8, test_cb)
+    sub = rospy.Subscriber('/status', Asv_state, state_cb)
 
     print "Wav"
     goal = SoundRequestGoal()
     goal.sound_request.sound = SoundRequest.PLAY_FILE
     goal.sound_request.command = SoundRequest.PLAY_ONCE
-    goal.sound_request.arg = os.path.join(roslib.packages.get_pkg_dir('sound_play'),'sounds') + "/NewVillageMoveCut.wav"
+    goal.sound_request.arg = os.path.join(roslib.packages.get_pkg_dir('sound_play'),'sounds') + "/classic.wav"
     goal.sound_request.volume = volume
 
     client.send_goal(goal, feedback_cb=feedback_cb)
@@ -53,11 +54,6 @@ def sound_play_client(volume=1.0):
         print "Now restarting the loop"
         if result.playing == False:
             client.send_goal(goal, feedback_cb=feedback_cb)
-
-    client.wait_for_result()
-    print client.get_result()
-    print "End wav"
-    print
 
 
 if __name__ == '__main__':
--- asv_teleop/src/asv_teleop_key.py
+++ asv_teleop/src/asv_teleop_key.py
@@ -39,8 +39,8 @@ class AckermannDriveKeyop:
 		# 	max_speed = float(args[0])
 		# 	max_steering_angle = float(args[1])
 		# else:
-		max_speed = 0.075
-		max_steering_angle = 80 / degree_per_rad 
+		max_speed = 0.05
+		max_steering_angle = 30 / degree_per_rad 
 
 		# if len(args) > 2:
 		# 	cmd_topic = '/' + args[2]
